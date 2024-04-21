730ac81864398361a31c2ebb5527c1e75fa96bd3
congle-engineer <congle-engineer@gmail.com>
2024-04-21 20:33:27 +0700

    context

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [CatsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 
 export class AppModule {}
