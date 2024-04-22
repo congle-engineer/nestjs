2961cada8ac6432f5a2c562c1f583caf7cf10569
congle-engineer <congle-engineer@gmail.com>
2024-04-22 18:56:23 +0700

    serve-static

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [CatsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+      exclude: ['api/(.*)']
+    })
+  ],
+  controllers: [AppController]
 })
 
 export class AppModule {}
