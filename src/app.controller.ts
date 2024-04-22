d254cb2180cd7ff450ca9a041a2684414310398c
congle-engineer <congle-engineer@gmail.com>
2024-04-22 21:33:05 +0700

    event-emitter

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -10,7 +10,7 @@ export class AppController {
   @Public()
   @ApiExcludeEndpoint()
   @Get()
-  getHello(): string {
+  getHello() {
     return this.appService.getHello();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,61 +1,13 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { APP_GUARD } from '@nestjs/core';
-import { AuthModule } from './auth/auth.module';
-import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
-import { DbModule } from './db/db.module';
-import { SeederModule } from './seeder/seeder.module';
-import { ConfigModule } from './config/config.module';
-import { ConfigService } from './config/config.service';
-import { RoleModule } from './role/role.module';
-import { LoggerMiddleware } from 'src/common/middleware/logger.middleware';
-import { ScheduleModule } from '@nestjs/schedule';
-import { UserModule } from './user/user.module';
-import {
-  I18nModule,
-  AcceptLanguageResolver,
-  QueryResolver,
-  HeaderResolver,
-} from 'nestjs-i18n';
-import * as path from 'path';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [
-    ThrottlerModule.forRoot([
-      {
-        ttl: ConfigService.App.ttl,
-        limit: ConfigService.App.limit,
-      },
-    ]),
-    ConfigModule,
-    DbModule,
-    SeederModule,
-    AuthModule,
-    RoleModule,
-    ScheduleModule.forRoot(),
-    I18nModule.forRoot({
-      fallbackLanguage: 'en',
-      loaderOptions: {
-        path: path.join(__dirname, '/message/'),
-        watch: true,
-      },
-      resolvers: [
-        { use: QueryResolver, options: ['lang'] },
-        AcceptLanguageResolver,
-        new HeaderResolver(['x-lang']),
-      ],
-    }),
-    UserModule,
-  ],
+  imports: [EventEmitterModule.forRoot(), OrdersModule],
   controllers: [AppController],
-  providers: [
-    AppService,
-    {
-      provide: APP_GUARD,
-      useClass: ThrottlerGuard,
-    },
-  ],
+  providers: [AppService]
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
