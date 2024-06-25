4dd3ff86a0871d8f95156dd3d477d8f21738d491
congle-engineer <congle-engineer@gmail.com>
2024-06-25 15:04:53 +0700

    Remove unnecessary code

--- src/utils/dto/update-util.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateUtilDto } from './create-util.dto';
-
-export class UpdateUtilDto extends PartialType(CreateUtilDto) {}
--- src/wallets/private-key.dto.ts
+++
@@ -1,8 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty } from 'class-validator';
-
-export class PrivateKeyDto {
-  @IsNotEmpty()
-  @ApiProperty()
-  privateKey: string;
-}
